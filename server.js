eadf04ae828016041a47fcdeb580e91ed74eb042
santorsm <matt.santorsola@gmail.com>
2020-12-27 14:01:11 -0800

    beginning feature 2

--- server.js
+++ server.js
@@ -7,11 +7,38 @@ const cors = require('cors');
 
 //set up our application
 const app = express();
-const PORT = process.env.PORT;
 app.use(cors());
 
+const PORT = process.env.PORT;
+
+app.get('/location', locationHandler);
+
+
+// Function Handlers
+function locationHandler(request, response){
+  const geoData = require('./data/location.json');
+  const city = request.query.city;
+  const locationData = new Location(city, geoData);
+
+  // response.send(geoData);
+
+  response.send(locationData);
+}
+
+//Constructor
+function Location(city, formatted, lat, lon){
+  this.search_query = city;
+  this.formatted_query = formatted;
+  this.latitude = lat;
+  this.longitude = lon;
+}
 
 //start our server
 app.listen(PORT, () => {
   console.log(`Now listening on port, ${PORT}`);
 });
+
+
+// app.use('*',(request, response) => {
+//   response.send('404. Sorry!');
+// });

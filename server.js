6c4edb2f7126e3828b12bd62b1da78b385874853
santorsm <matt.santorsola@gmail.com>
2021-01-07 23:51:35 -0800

    initial attempt

--- server.js
+++ server.js
@@ -7,19 +7,54 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
+
+const client = new pg.Client(process.env.DATABASE_URL);
 
 //set up our application
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
 app.use(cors());
 
-// Routes
+
+client.on('error', err => {
+  throw err;
+});
+
+// app.get('/',(request, response) => {
+//   response.status(200).send('Hello World');
+// });
+
+app.get('/add', (request, response) => {
+  let city = request.query.city;
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
+  let display = request.formatted_query;
+
+  let SQL = 'INSERT INTO location (city, lat, lon, display) VALUES ($1, $2, $3, $4) RETURNING *';
+
+  let safeValues = [city, lat, lon, display];
+
+  console.log(safeValues);
+
+  client.query(SQL, safeValues)
+    .then( results => {
+      response.status(200).json(results);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+});
+
+
+// // Routes
 app.get('/', homeHandler);
 app.get('/location', locationHandler);
-app.get('/weather', weatherHandler);
+// app.get('/weather', weatherHandler);
 app.use('*', errorHandler);
 
-// Function Handlers
+// // Function Handlers
 function homeHandler (request, response){
   response.status(200).send('Nosce te ipsum');
 }
@@ -46,48 +81,57 @@ function locationHandler(request, response){
 
 }
 
-function weatherHandler (request, response){
-  // const weatherData = require('./data/weather.json');
-  // let lat = request.query.data.latitude;
-  // let long = request.query.data.longitude;
+// function weatherHandler (request, response){
+//   // const weatherData = require('./data/weather.json');
+//   // let lat = request.query.data.latitude;
+//   // let long = request.query.data.longitude;
 
-  const key = process.env.WEATHER_API_KEY;
-  const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.latitude}&lon=${request.query.longitude}&key=${key}&lang=en&units=I&days=8`;
+//   const key = process.env.WEATHER_API_KEY;
+//   const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.latitude}&lon=${request.query.longitude}&key=${key}&lang=en&units=I&days=8`;
 
-  // const weatherArray = [];
+//   // const weatherArray = [];
 
-  let weatherData;
+//   let weatherData;
 
-  superagent.get(weatherUrl)
-    .then( data => {
-      // data.weather.description;
-      weatherData = data.body.data.map(dailyForecast => {
-        return new Weather (dailyForecast);
-      });
-      console.log(weatherData);
-      response.status(200).send(weatherData);
+//   superagent.get(weatherUrl)
+//     .then( data => {
+//       // data.weather.description;
+//       weatherData = data.body.data.map(dailyForecast => {
+//         return new Weather (dailyForecast);
+//       });
+//       console.log(weatherData);
+//       response.status(200).send(weatherData);
 
-    });
+//     });
 
-}
+// }
 
 
 
 //Constructors
-function Location(city, locationInfo){
+function Location(city, locationData){
   this.search_query = city;
-  this.formatted_query = locationInfo.display_name;
-  this.latitude = locationInfo.lat;
-  this.longitude = locationInfo.lon;
+  this.formatted_query = locationData.display_name;
+  this.latitude = locationData.lat;
+  this.longitude = locationData.lon;
 }
 
-function Weather (data) {
-  this.forecast = data.weather.description;
-  let date = Date.parse(data.datetime);
-  this.time = new Date(date).toDateString();
-}
+// function Weather (data) {
+//   this.forecast = data.weather.description;
+//   let date = Date.parse(data.datetime);
+//   this.time = new Date(date).toDateString();
+// }
 
-//start our server
-app.listen(PORT, () => {
-  console.log(`Now listening on port, ${PORT}`);
-});
+// //start our server
+// app.listen(PORT, () => {
+//   console.log(`Now listening on port, ${PORT}`);
+// });
+
+//start database
+client.connect()
+  .then( () =>{
+    app.listen(PORT, () => {
+      console.log(`Now listening on port, ${PORT}`);
+      console.log(`Connected to database, ${client.connectionParameters.database}`);
+    });
+  });
